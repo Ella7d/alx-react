a68a96be88af1bef34c0d5a34b794ae1116e66be
root <ella.debesai@gmail.com>
2023-07-31 19:37:06 +0000

    react props

--- 0x02-react_props/task_2/dashboard/src/App/App.js
+++ 0x02-react_props/task_2/dashboard/src/App/App.js
@@ -1,20 +1,29 @@
-import logo from './Holberton-logo.jpg';
+import React from 'react';
 import './App.css';
+import logo from '../assets/holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
-    <div className='App'>
-			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
-			</div>
-			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
-			</div>
-			<div className='App-footer'>
-				<p>Copyright 2020 - holberton School</p>
-			</div>
-		</div>
+    <>
+      <header className='App-header'>
+        <img src={logo} alt='logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <main role='main' className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='button'>OK</button>
+      </main>
+      <footer className='App-footer'>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </>
   );
 }
 
