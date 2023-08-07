616f9feee3ea3f772ae138787ae19b42384954fb
root <ella.debesai@gmail.com>
2023-08-07 17:47:20 +0000

    inline styling

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,21 +1,57 @@
-import React from "react";
-import { shallow } from "enzyme";
-import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
-import BodySection from "./BodySection";
-import { StyleSheetTestUtils } from "aphrodite";
+import React from 'react';
+import CourseListRow from './CourseListRow';
+import PropTypes from 'prop-types';
+import CourseShape from './CourseShape';
+import './CourseList.css';
+import { StyleSheet, css } from 'aphrodite';
 
-beforeEach(() => {
-  StyleSheetTestUtils.suppressStyleInjection();
-});
-afterEach(() => {
-  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+const CourseList = ({ listCourses }) => {
+  return (
+    <table className={css(styles.courseList)}>
+      <thead>
+        <CourseListRow textFirstCell='Available courses' isHeader={true} />
+        <CourseListRow
+          textFirstCell='Course name'
+          textSecondCell='Credit'
+          isHeader={true}
+        />
+      </thead>
+      <tbody>
+        {listCourses.length === 0 && (
+          <CourseListRow
+            textFirstCell='No course available yet'
+            isHeader={false}
+          />
+        )}
+
+        {listCourses.map((course) => (
+          <CourseListRow
+            key={course.id}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+            isHeader={false}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+const styles = StyleSheet.create({
+  courseList: {
+    width: '95%',
+    margin: 'auto',
+    border: '1px solid gray',
+    borderCollapse: 'collapse',
+  },
 });
 
-describe("BodySectionWithMarginBottom tests", () => {
-  it("should apply margin bottom to child component", () => {
-    const wrapper = shallow(<BodySectionWithMarginBottom title="test title" />);
-
-    expect(wrapper.find(BodySection)).toHaveLength(1);
-    expect(wrapper.find(BodySection).html()).toEqual('<div class="bodySection"><h2>test title</h2></div>');
-  });
-});
+export default CourseList;
